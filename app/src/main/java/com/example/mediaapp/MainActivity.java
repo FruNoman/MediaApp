e90a24689c028b36f303c2c86d8cbebecbb7fd4b
dmytro.a.frolov <dmytro.a.frolov@globallogic.com>
2021-02-01 16:19:53 +0200

    add next bvuuton

--- app/src/main/java/com/example/mediaapp/MainActivity.java
+++ app/src/main/java/com/example/mediaapp/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.mediaapp;
 
 import android.Manifest;
+import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.chibde.visualizer.SquareBarVisualizer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public Uri uri;
     public  SquareBarVisualizer squareBarVisualizer;
     public Button play;
+    public Button stop;
+    public Button next;
+    public TextView trackName;
+    public int nowPlaying = 0;
+    public List<Song> songs = new ArrayList<>();
+   public  int seekLength = 0;
+
     public   MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +52,14 @@ public class MainActivity extends AppCompatActivity {
         uri = Uri.parse("/storage/emulated/10/samples/audio/A_000001_02_Tetanus.mp3");
         squareBarVisualizer = findViewById(R.id.visualizer);
         play = findViewById(R.id.play);
+        stop = findViewById(R.id.stop);
+        next = findViewById(R.id.next);
+        trackName = findViewById(R.id.track);
+
+        setSongsList();
+
         mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
             mediaPlayer.setDataSource(this,uri);
         } catch (IOException e) {
@@ -48,10 +69,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                    squareBarVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
-                } catch (IOException e) {
+                    playSong(nowPlaying);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               pauseSong();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    nextSong();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -67,5 +104,68 @@ public class MainActivity extends AppCompatActivity {
         squareBarVisualizer.setGap(2);
 
 // Set your media player to the visualizer.
+    }
+
+
+
+    void setSongsList(){
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        String sortOrder = MediaStore.MediaColumns.DISPLAY_NAME+"";
+
+        String[] projection = {
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+
+
+        Cursor cursor = getApplicationContext().getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                sortOrder);
+
+        while(cursor.moveToNext()){
+            songs.add(new Song(cursor.getString(0),cursor.getString(1),cursor.getString(2),Long.parseLong(cursor.getString(3))));
+        }
+    }
+
+    void playSong(int index) throws Exception {
+        if(index != nowPlaying){
+            seekLength = 0;
+        }
+        nowPlaying = index;
+        playSong();
+    }
+
+    void playSong() throws Exception {
+        mediaPlayer.reset();
+        Uri path = Uri.parse(songs.get(nowPlaying).getData());
+        mediaPlayer.setDataSource(String.valueOf(path));
+        mediaPlayer.prepare();
+        mediaPlayer.seekTo(seekLength);
+        mediaPlayer.start();
+        squareBarVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
+
+    }
+
+    void pauseSong(){
+        mediaPlayer.pause();
+        seekLength = mediaPlayer.getCurrentPosition();
+    }
+
+
+    void nextSong() throws Exception {
+        nowPlaying = nowPlaying+1;
+        if (nowPlaying == songs.size()){
+            nowPlaying = 0;
+        }
+        seekLength = 0;
+        if(mediaPlayer.isPlaying()){
+            playSong();
+        }
     }
 }
---
+++ app/src/main/java/com/example/mediaapp/Song.java
@@ -0,0 +1,21 @@
+package com.example.mediaapp;
+
+import android.provider.MediaStore;
+
+public class Song {
+    private String artist;
+    private String title;
+    private String data;
+    private long duration;
+
+    public Song(String artist, String title, String data, long duration) {
+        this.artist = artist;
+        this.title = title;
+        this.data = data;
+        this.duration = duration;
+    }
+
+    public String getData(){
+        return this.data;
+    }
+}
